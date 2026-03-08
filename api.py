6bf116df8c5f9de375d32f1783d3d44c4f84e0da
Sophon96 <71684640+Sophon96@users.noreply.github.com>
2026-03-08 14:52:50 -0700

    feat: add main snippet to api.py

--- api.py
+++ api.py
@@ -171,3 +171,9 @@ async def inference_project(req: InferenceRequest):
             raise HTTPException(400, str(e))
 
     return result
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("api:app", host="0.0.0.0", port=8000)
